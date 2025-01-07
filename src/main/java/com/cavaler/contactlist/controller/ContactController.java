99d00d6c7c178649a39c1f3e8ce2ffdb30020db7
FranzBurneo <franzdavidburneo@gmail.com>
2025-01-06 22:49:23 -0500

    feat: find contact by id

--- src/main/java/com/cavaler/contactlist/controller/ContactController.java
+++ src/main/java/com/cavaler/contactlist/controller/ContactController.java
@@ -4,6 +4,7 @@ import com.cavaler.contactlist.entity.Contact;
 import com.cavaler.contactlist.repository.ContactRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,9 +13,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class ContactController {
 
     @Autowired
-    private ContactRepository contactRepository;
+    public ContactRepository contactRepository;
     @GetMapping
     Iterable<Contact> list(){
         return contactRepository.findAll();
+    }
+
+    @GetMapping("{id}")
+    public Contact get(@PathVariable() Integer id){
+        return contactRepository
+                .findById(id)
+                .orElse(null);
     }
 }
